158c8445a5cf32b7f6b2767e40e166537f292a3a
matineghazanfari <m.ghazanfari@bluteam.ir>
2024-11-17 12:28:46 +0330

    init footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button, Layout } from "antd";
+
+export default function MyFooter() {
+  const { Footer } = Layout;
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      ©{new Date().getFullYear()} Matineh Ghazanfari. All Rights Reserved.
+      <p>
+        Looking to get in touch?{" "}
+        <a href="/contact-me" className="text-blue-400">
+          Contact Me
+        </a>
+      </p>
+      <Button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+        Back to Top ↑
+      </Button>
+    </Footer>
+  );
+}
