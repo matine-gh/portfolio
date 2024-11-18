5df4977785e1ceb835258a48c2fa548f8cf1a87d
matineghazanfari <m.ghazanfari@bluteam.ir>
2024-11-18 17:26:47 +0330

    add css in js for antd

--- src/app/container.tsx
+++ src/app/container.tsx
@@ -3,11 +3,12 @@ import { MyHeader } from "@/components/Header";
 import { Breadcrumb, Layout } from "antd";
 import MyFooter from "@/components/Footer";
 import { ReactNode } from "react";
+import { StyleProvider } from "@ant-design/cssinjs";
 
 export default function Container({ children }: { children: ReactNode }) {
   const { Content } = Layout;
   return (
-    <>
+    <StyleProvider>
       <MyHeader />
       <Content style={{ padding: "0 48px" }}>
         <Breadcrumb style={{ margin: "16px 0" }}>
@@ -27,6 +28,6 @@ export default function Container({ children }: { children: ReactNode }) {
         </div>
       </Content>
       <MyFooter />
-    </>
+    </StyleProvider>
   );
 }
