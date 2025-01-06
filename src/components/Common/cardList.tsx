47a76e629cdfc77e9dd9b93cb757dd9164ec1401
matineghazanfari <m.ghazanfari@bluteam.ir>
2025-01-06 15:35:32 +0330

    improvement card style

--- src/components/Common/cardList.tsx
+++ src/components/Common/cardList.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Card, Col, Row } from "antd";
 import { pages } from "@/lib";
+import Link from "next/link";
 
 export default function CardList() {
   return (
-    <Row>
+    <Row gutter={[16, 16]}>
       {pages.map((card) => (
-        <Col key={card.title}>
-          <Card hoverable={true} style={{ width: 240 }} cover={card.cover}>
-            {/*<Meta title="Europe Street beat" description="www.instagram.com" />*/}
-            <p>{card.title}</p>
-          </Card>
-        </Col>
+        <Link href={card.path}>
+          <Col key={card.title}>
+            <Card hoverable={true} style={{ width: 240 }} cover={card.cover}>
+              {/*<Meta title="Europe Street beat" description="www.instagram.com" />*/}
+              <p>{card.title}</p>
+            </Card>
+          </Col>
+        </Link>
       ))}
     </Row>
   );
